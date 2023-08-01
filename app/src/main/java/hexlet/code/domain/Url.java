1338ec0ada5abdf2fe1095dd322d18226648f18b
Andrew <rassandev@gmail.com>
2023-08-01 20:18:49 +0300

    add class Url.java

---
+++ app/src/main/java/hexlet/code/domain/Url.java
@@ -0,0 +1,40 @@
+package hexlet.code.domain;
+
+import io.ebean.Model;
+import io.ebean.annotation.WhenCreated;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public final class Url extends Model {
+    @Id
+    private long id;
+
+    private String name;
+
+    @WhenCreated
+    private Instant createdAt;
+
+    public Url() {
+
+    }
+
+    public Url(String name, Instant createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Instant getCreatedAt() {
+        return this.createdAt;
+    }
+}
