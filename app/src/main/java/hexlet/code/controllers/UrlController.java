dfa4c22941bb978a8eab6d5a3ace233f006704db
Andrew <rassandev@gmail.com>
2023-08-14 14:53:46 +0300

    add handler for url checks to UrlController.java class

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,10 +1,17 @@
 package hexlet.code.controllers;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.UrlCheck;
 import hexlet.code.domain.query.QUrl;
 import io.ebean.PagedList;
 import io.javalin.http.Handler;
 import io.javalin.http.NotFoundResponse;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestParsingException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -49,11 +56,11 @@ public final class UrlController {
         } catch (MalformedURLException e) {
             ctx.sessionAttribute("flash", "Некорректный URL");
             ctx.sessionAttribute("flash-type", "danger");
-            ctx.render("index.html");
+            ctx.redirect("/");
             return;
         }
 
-        String normalizedUrl = receivedUrl.getProtocol() + "://" + receivedUrl.getAuthority();
+        String normalizedUrl = "%s://%s".formatted(receivedUrl.getProtocol(), receivedUrl.getAuthority());
 
         boolean urlExists =
                 new QUrl()
@@ -87,5 +94,44 @@ public final class UrlController {
 
         ctx.attribute("url", urlById);
         ctx.render("urls/show.html");
+    };
+
+    public static Handler createCheck = ctx -> {
+        int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
+
+        Url urlById = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+
+        if (urlById == null) {
+            throw new NotFoundResponse();
+        }
+
+        try {
+            HttpResponse<String> response = Unirest.get(urlById.getName()).asString();
+            String body = response.getBody();
+
+            Document doc = Jsoup.parse(body);
+
+            int statusCode = response.getStatus();
+            String title = doc.title();
+            Element h1Element = doc.selectFirst("h1");
+            String h1 = h1Element == null ? "" : h1Element.text();
+            Element descriptionElement = doc.selectFirst("meta[content]");
+            String description = descriptionElement == null ? "" : descriptionElement.attributes().get("content");
+
+            UrlCheck newUrlCheck = new UrlCheck(statusCode, title, h1, description);
+
+            urlById.getUrlChecks().add(newUrlCheck);
+            urlById.save();
+
+            ctx.sessionAttribute("flash", "Страница успешно проверена");
+            ctx.sessionAttribute("flash-type", "success");
+        } catch (UnirestParsingException e) {
+            ctx.sessionAttribute("flash", "Ошибка проверки страницы");
+            ctx.sessionAttribute("flash-type", "danger");
+        }
+
+        ctx.redirect("/urls/" + urlById.getId());
     };
 }
